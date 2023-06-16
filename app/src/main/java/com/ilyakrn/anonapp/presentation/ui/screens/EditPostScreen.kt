0a56e84fe30449cd97b493c6db498997108a1e71
IlyaKrn <ilyakornoukhov@gmail.com>
2023-06-16 17:27:43 +0300

    init

---
+++ app/src/main/java/com/ilyakrn/anonapp/presentation/ui/screens/EditPostScreen.kt
@@ -0,0 +1,11 @@
+package com.ilyakrn.anonapp.presentation.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview
+@Composable
+fun EditPostScreen() {
+
+}
